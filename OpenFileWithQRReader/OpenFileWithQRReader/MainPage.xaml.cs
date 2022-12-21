d8531c0285ee47286c388f1417a1133f541600bf
Isti Olah <iolah2@gmail.com>
2022-12-21 11:16:33 +0100

    2 QrScanning mobile starting

---
+++ OpenFileWithQRReader/OpenFileWithQRReader/MainPage.xaml.cs
@@ -0,0 +1,82 @@
+﻿using OpenFileWithQRReader.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using ZXing.Net.Mobile.Forms;
+
+namespace OpenFileWithQRReader
+{
+    //https://stackoverflow.com/questions/69754142/xamarin-android-camera-permissions-with-zxing-net-mobile-only-works-after-app-re
+    public partial class MainPage: ContentPage //: ZXingScannerView //
+                                               //ContentPage, INotifyPropertyChanged
+    {
+        //private bool _isScanning;
+
+        //public bool IsScanning
+        //{
+        //    get { return _isScanning; }
+        //    set { _isScanning = value; }
+        //}
+
+        public MainPage()
+        {
+            InitializeComponent();
+            //IsScanning = false;
+            //BindingContext = this;
+            //Indicator.Start();
+        }
+
+        //https://stackoverflow.com/questions/54750040/how-to-restart-the-camera-in-zxing-after-it-finds-the-qr-code?rq=1
+
+        //public void startScan()
+        //{ //use this when you want to resume the camera
+        //    if (scannerView != null)
+        //    {
+        //        scannerView.setResultHandler(this);
+        //        scannerView.startCamera();
+        //        rescan();
+        //    }
+        //}
+
+        //public void stopScan()
+        //{ //use this when you want to stop scanning
+        //  // it is very important to do that,
+        //  // because the camera will keep scanning codes in background
+        //    if (scannerView != null)
+        //    {
+        //        scannerView.stopCameraPreview();
+        //        scannerView.stopCamera();
+        //    }
+        //}
+
+        //public void rescan()
+        //{
+        //    if (scannerView != null)
+        //    {
+        //        scannerView.resumeCameraPreview(this);
+        //    }
+        //}
+
+        private async void btnScan_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                var scanner = DependencyService.Get<IQrScanningService>();
+                var result = await scanner.ScanAsync();
+                if (result != null)
+                {
+                    txtBarcode.Text = result;
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+        }
+    }
+}
---
+++ OpenFileWithQRReader/OpenFileWithQRReader/ScanBarCode.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+using ZXing.Net.Mobile.Forms;
+
+namespace OpenFileWithQRReader
+{
+    public class ScanBarCode : ContentPage
+    {
+        StackLayout stackMainlayout;
+        public ScanBarCode()
+        {
+            stackMainlayout = new StackLayout
+            {
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+                VerticalOptions = LayoutOptions.FillAndExpand,
+                Orientation = StackOrientation.Vertical
+            };
+            ZXingScannerPage scanPage;
+            Button btnScan = new Button
+            {
+                Text = "Scan",
+                HorizontalOptions = LayoutOptions.Center,
+                VerticalOptions = LayoutOptions.Center
+            };
+            btnScan.Clicked += async (a, s) =>
+            {
+                scanPage = new ZXingScannerPage();
+                // Use current Navigation directly    
+                await Navigation.PushAsync(scanPage);
+                //https://social.msdn.microsoft.com/Forums/en-US/e61054c5-e2d7-4468-a900-532aca3d1a1b/zxingscannerpage-is-not-opening-to-read-qr-code?forum=xamarinios
+                scanPage.OnScanResult += (result) =>
+                {
+                    scanPage.IsScanning = false;
+                    Device.BeginInvokeOnMainThread(async () =>
+                    {
+                        await Navigation.PopModalAsync();
+                        await DisplayAlert("Scanned Barcode", result.Text + " , " + result.BarcodeFormat + " ," + result.ResultPoints[0].ToString(), "OK");
+                    });
+                };
+                //await Navigation.PushModalAsync(scanPage);
+            };
+            stackMainlayout.Children.Add(btnScan);
+            Content = stackMainlayout;
+        }
+    }
+}
